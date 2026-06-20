66e7c6584934b38f46d37dc1ad4bc225d52337ad
SorChannorakpitou <channorakpitou.sor@student.cadt.edu.kh>
2026-06-20 13:49:53 +0700

    add dependency

--- back/src/server.js
+++ back/src/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import articleRouter from "./routes/articleRoutes.js";
 import journalistRouter from "./routes/journalistRoutes.js";
